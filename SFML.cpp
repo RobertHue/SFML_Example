5a4db88564f42bbc3d5dd5f5e2d7a5bb0a98662a
RobertHue <robert.huemmer@gmail.com>
2019-03-03 17:21:14 +0100

    update: particleSystem at mouse cursor and loading of a tilemap

--- SFML.cpp
+++ SFML.cpp
@@ -1,8 +1,186 @@
 #include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <boost/filesystem.hpp>
+#include <SFML/Audio.hpp>
+
+
+class ParticleSystem : public sf::Drawable, public sf::Transformable
+{
+public:
+
+	ParticleSystem(unsigned int count) :
+		m_particles(count),
+		m_vertices(sf::Points, count),
+		m_lifetime(sf::seconds(3.f)),
+		m_emitter(0.f, 0.f)
+	{
+	}
+
+	void setEmitter(sf::Vector2f position)
+	{
+		m_emitter = position;
+	}
+
+	void update(sf::Time elapsed)
+	{
+		for (std::size_t i = 0; i < m_particles.size(); ++i)
+		{
+			// update the particle lifetime
+			Particle& p = m_particles[i];
+			p.lifetime -= elapsed;
+
+			// if the particle is dead, respawn it
+			if (p.lifetime <= sf::Time::Zero)
+				resetParticle(i);
+
+			// update the position of the corresponding vertex
+			m_vertices[i].position += p.velocity * elapsed.asSeconds();
+
+			// update the alpha (transparency) of the particle according to its lifetime
+			float ratio = p.lifetime.asSeconds() / m_lifetime.asSeconds();
+			m_vertices[i].color.a = static_cast<sf::Uint8>(ratio * 255);
+		}
+	}
+
+private:
+
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
+	{
+		// apply the transform
+		states.transform *= getTransform();
+
+		// our particles don't use a texture
+		states.texture = NULL;
+
+		// draw the vertex array
+		target.draw(m_vertices, states);
+	}
+
+private:
+
+	struct Particle
+	{
+		sf::Vector2f velocity;
+		sf::Time lifetime;
+	};
+
+	void resetParticle(std::size_t index)
+	{
+		// give a random velocity and lifetime to the particle
+		float angle = (std::rand() % 360) * 3.14f / 180.f;
+		float speed = (std::rand() % 50) + 50.f;
+		m_particles[index].velocity = sf::Vector2f(std::cos(angle) * speed, std::sin(angle) * speed);
+		m_particles[index].lifetime = sf::milliseconds((std::rand() % 2000) + 1000);
+
+		// reset the position of the corresponding vertex
+		m_vertices[index].position = m_emitter;
+	}
+
+	std::vector<Particle> m_particles;
+	sf::VertexArray m_vertices;
+	sf::Time m_lifetime;
+	sf::Vector2f m_emitter;
+};
+class TileMap : public sf::Drawable, public sf::Transformable
+{
+public:
+
+	bool load(const std::string& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height)
+	{
+		// load the tileset texture
+		if (!m_tileset.loadFromFile(tileset))
+			return false;
+
+		// resize the vertex array to fit the level size
+		m_vertices.setPrimitiveType(sf::Quads);
+		m_vertices.resize(width * height * 4);
+
+		// populate the vertex array, with one quad per tile
+		for (unsigned int i = 0; i < width; ++i)
+			for (unsigned int j = 0; j < height; ++j)
+			{
+				// get the current tile number
+				int tileNumber = tiles[i + j * width];
+
+				// find its position in the tileset texture
+				int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
+				int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
+
+				// get a pointer to the current tile's quad
+				sf::Vertex* quad = &m_vertices[(i + j * width) * 4];
+
+				// define its 4 corners
+				quad[0].position = sf::Vector2f(i * tileSize.x, j * tileSize.y);
+				quad[1].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
+				quad[2].position = sf::Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
+				quad[3].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
+
+				// define its 4 texture coordinates
+				quad[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
+				quad[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
+				quad[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
+				quad[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
+			}
+
+		return true;
+	}
+
+private:
+
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
+	{
+		// apply the transform
+		states.transform *= getTransform();
+
+		// apply the tileset texture
+		states.texture = &m_tileset;
+
+		// draw the vertex array
+		target.draw(m_vertices, states);
+	}
+
+	sf::VertexArray m_vertices;
+	sf::Texture m_tileset;
+};
 
 int main()
 {
-	sf::Window window(sf::VideoMode(800, 600), "My window");
+	float factor = 2.f;
+	sf::RenderWindow window(sf::VideoMode(512*factor, 256* factor), "My 2d Drawings/Sprites");
+	
+	sf::Music music;
+	if (!music.openFromFile("..\\Zelda_SwordSearch.wav"))
+		return -1; // error
+	music.play();
+
+
+	// create the particle system
+	ParticleSystem particles(1000);
+
+	// create a clock to track the elapsed time
+	sf::Clock clock;
+
+
+
+	// define the level with an array of tile indices
+	const int level[] =
+	{
+		0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0,
+		1, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3,
+		0, 1, 0, 0, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 0, 0,
+		0, 1, 1, 0, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2, 0, 0,
+		0, 0, 1, 0, 3, 0, 2, 2, 0, 0, 1, 1, 1, 1, 2, 0,
+		2, 0, 1, 0, 3, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1,
+		0, 0, 1, 0, 3, 2, 2, 2, 0, 0, 0, 0, 1, 1, 1, 1,
+	};
+
+	// create the tilemap from the level definition
+	TileMap map;
+	if (!map.load("..\\graphics-vertex-array-tilemap-tileset.png", sf::Vector2u(32, 32), level, 16, 8))
+		return -1;
+
 
 	// run the program as long as the window is open
 	while (window.isOpen())
@@ -15,6 +193,58 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+
+		//boost::filesystem::path full_path(boost::filesystem::current_path());
+		//std::cout << "Current path is : " << full_path << std::endl;
+
+		boost::filesystem::path fs_path("..\\Link.png");
+		sf::Texture texture;
+		if (!texture.loadFromFile(fs_path.string())) {
+			// error...
+			std::cerr << "Error loading texture. Either not the right name or missing..." << std::endl;
+		}
+
+		sf::Sprite sprite;
+		sprite.setTexture(texture);
+		sprite.setColor(sf::Color(215, 122, 155)); // change color
+
+		// position
+		sprite.setPosition(sf::Vector2f(10.f, 50.f)); // absolute position
+		sprite.move(sf::Vector2f(5.f, 10.f)); // offset relative to the current position
+
+
+		// scale
+		//sprite.setScale(sf::Vector2f(0.5f, 2.f)); // absolute scale factor
+		sprite.scale(sf::Vector2f(factor, factor)); // factor relative to the current scale
+		map.setScale(sf::Vector2f(factor, factor));
+
+
+		// make the particle system emitter follow the mouse
+		sf::Vector2i mouse = sf::Mouse::getPosition(window);
+		particles.setEmitter(window.mapPixelToCoords(mouse));
+
+		// update it
+		sf::Time elapsed = clock.restart();
+		particles.update(elapsed);
+
+		////////////////
+		// clear the window with black color
+		window.clear(sf::Color::Black);
+		/////////////////////////////////////////////////////////////
+		// , sf::IntRect(32, 7, 13, 16)
+		// do something with the sprites...
+
+		// inside the main loop, between window.clear() and window.display()
+		//window.draw(sprite);
+		window.draw(map);
+		window.draw(particles);
+
+		/////////////////////////////////////////////////////////////
+		// draw everything here...
+		// window.draw(...);
+		// end the current frame
+		window.display();
+		////////////////
 	}
 
     return 0;
